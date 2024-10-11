f164979b30f993ea980f67958d30e5e38762406d
DarrenRohan <darrenrohan64@gmail.com>
2024-10-11 11:01:47 -0700

    Used my lab 4-2 code, updating that code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
     total = 0
+    i = 0
+
+    while i < 5:
+        numin = int(input("Enter number: "))
+        total += numin
+        i += 1
+    print (total)
 
     ########################################
     # Do not delete the return statement
